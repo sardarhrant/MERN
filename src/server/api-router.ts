2dc256f555241118f6d7e70096dc678121adedd8
Hrant Sardaryan <hrant_sardaryan@epam.com>
2023-06-29 11:56:14 +0400

    Fetching Fata From DB

--- src/server/api-router.ts
+++ src/server/api-router.ts
@@ -1,12 +1,34 @@
 import express from "express";
 import testData from "../test-data.json";
 import cors from "cors";
+import { connectClient } from "./db";
 
 const router = express.Router();
 router.use(cors());
 
-router.get('/contests', (req, res) => {
-    res.send({ contests: testData });
+router.get('/contests', async (req, res) => {
+    const client = await connectClient();
+    const contests = await client.collection("contests")
+        .find()
+        .project({
+            id: 1,
+            categoryName: 1,
+            contestName: 1,
+            _id: 0
+        })
+        .toArray();
+
+    res.send({ contests });
+});
+
+router.get("/contest/:contestId", async (req, res) => {
+    const client = await connectClient();
+    const singleContest = await client.collection("contests")
+        .findOne({
+            id: req.params.contestId
+        });
+
+    res.send({ contest: singleContest });
 });
 
 export default router;
